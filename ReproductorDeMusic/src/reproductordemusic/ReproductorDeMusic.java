a7428eb8190a2c7331ba2d6c81a5fa71a205b018
JhonCode <jjv20618@gmail.com>
2023-06-06 19:21:51 -0600

    Add files via upload

---
+++ ReproductorDeMusic/src/reproductordemusic/ReproductorDeMusic.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package reproductordemusic;
+/**
+ *
+ * @author Jonathan
+ */
+public class ReproductorDeMusic {
+    public static void main(String[] args) {
+        Interfaz i = new Interfaz();
+        i.setLocationRelativeTo(null);
+        i.setVisible(true);
+    }
+}
